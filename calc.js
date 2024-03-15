a77c04cfdbcc14af1726a1a433bea099dfc306ae
najia <najiaknizi199@gmail.com>
2024-03-15 10:15:33 +0000

    fonction afficher

--- calc.js
+++ calc.js
@@ -1,1 +1,75 @@
-document.write(document.getElementsByClassName("num1")[4].value);
+
+
+function clickeNum1(){
+    document.getElementById("numbers").value= document.getElementById("numbers").value+1;
+     }
+    
+
+     function clickeNum2(){
+        document.getElementById("numbers").value= document.getElementById("numbers").value+2;
+     }
+
+     function clickeNum3(){
+        document.getElementById("numbers").value= document.getElementById("numbers").value+3;
+     }
+
+     function clickeNum4(){
+        document.getElementById("numbers").value= document.getElementById("numbers").value+4;
+     }
+
+     function clickeNum5(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+5;
+     }
+
+     function clickeNum6(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+6;
+     }
+
+
+     function clickeNum7(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+7;
+     }
+
+     function clickeNum8(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+8;
+     }
+
+     function clickeNum9(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+9;
+     }
+
+     function clickeNum0(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+0;
+     }
+     function clickEgal(){
+       // document.getElementById("numbers").value=document.getElementById("numbers").value+"=";
+     }
+     function clickerplus(){
+       document.getElementById("numbers").value=document.getElementById("numbers").value+"+";
+        
+
+     }
+     function clickerMoins(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+"-";
+ 
+      }
+    function clickparant1(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+"(";
+    }
+
+    function clickparant2(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+")";
+    }
+    function clickpoint(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+".";
+    }
+    function clickeDivision(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+"/";
+    }
+    function clickerMultiplication(){
+        document.getElementById("numbers").value=document.getElementById("numbers").value+"*";
+    }
+    function calculer(){
+         document.getElementById("numbers").innerHTML=document.getElementById("numbers").value;
+    }
+    
